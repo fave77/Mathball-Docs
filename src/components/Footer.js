c32d916ccfa4e3738ecd5e586be88ba0dadec2ce
devRD <ratnabalidutta26@gmail.com>
2019-03-10 20:57:50 +0530

    Home Page Update

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../stylesheets/footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className = "footer-container">
+        <div className = "container-fluid">
+          <div className = "footer-links">
+              <a href = "#About" target = "_blank" rel = "noopener noreferrer">Examples</a>
+              <a href = "#About" target = "_blank" rel = "noopener noreferrer">About</a>
+          </div>
+          <div className = "para-links">
+              <p>MIT License 2019 - <a href = "https://github.com/pbiswas101" target = "_blank" rel = "noopener noreferrer">Priyabrata Biswas. </a> </p>
+              <p>Made with <span style= {{color:"red"}}> &hearts; </span> and JavaScript by the Mathball team!</p>
+            </div>
+          </div>
+      </footer>
+    );
+  }
+}
+  export default Footer;
