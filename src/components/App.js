84fad90b09ad46aa3d2f934ddc63c9254be4c7ec
Priyabrata Biswas <pbiswas101b@gmail.com>
2019-03-11 20:53:38 +0530

    fixing Travis CI build

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar";
-import Header from "./Heaier";
+import Header from "./Header";
 import MainContent from "./Main-Content";
 import Footer from "./Footer";
 
