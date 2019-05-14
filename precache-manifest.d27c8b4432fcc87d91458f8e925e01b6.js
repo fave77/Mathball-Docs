76443388068a16f5c303b54829ec986e83a749fa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-14 13:42:41 +0000

    Deploy pbiswas101/Mathball-Docs to github.com/pbiswas101/Mathball-Docs.git:gh-pages

---
+++ precache-manifest.d27c8b4432fcc87d91458f8e925e01b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "169ab6629a341734c329",
+    "url": "/Mathball-Docs/static/css/main.8544b3da.chunk.css"
+  },
+  {
+    "revision": "169ab6629a341734c329",
+    "url": "/Mathball-Docs/static/js/main.169ab662.chunk.js"
+  },
+  {
+    "revision": "c78cfeaea8bb95349a34",
+    "url": "/Mathball-Docs/static/js/runtime~main.c78cfeae.js"
+  },
+  {
+    "revision": "19bf31e220e7bbf523da",
+    "url": "/Mathball-Docs/static/css/2.9081468a.chunk.css"
+  },
+  {
+    "revision": "19bf31e220e7bbf523da",
+    "url": "/Mathball-Docs/static/js/2.19bf31e2.chunk.js"
+  },
+  {
+    "revision": "4730e0134a85c8278ad2311fa252f657",
+    "url": "/Mathball-Docs/static/media/logo.4730e013.png"
+  },
+  {
+    "revision": "60f0ec63fc8ad604d2687a54f3a74cdd",
+    "url": "/Mathball-Docs/index.html"
+  }
+];
